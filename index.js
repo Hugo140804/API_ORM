af14fbda617deb34a99b75251d58d458d0e34c29
Hugo <hgugi3nsn@gmail.com>
2026-07-22 10:49:54 +0700

    Update index.js

--- index.js
+++ index.js
@@ -20,3 +20,52 @@ db.sequelize.sync()
     .catch((err) => {
         console.log(err);
     });
+
+app.post('/komik', async (req, res) => {
+        const data = req.body;
+        try {
+            const komik = await db.Komik.create(data);
+            res.send(komik);
+        } catch (error) {
+            res.send(error);
+        }
+    
+});
+
+app.get('/komik', async (req, res) => {
+    try {
+        const komik = await db.Komik.findAll();
+        res.send(komik);
+    } catch (error) {
+        res.send(error);
+    }
+});
+
+app.put('/komik/:id', async (req, res) => {
+    const id = req.params.id;
+    const data = req.body;
+    try {
+        const komik = await db.Komik.findByPk(id);
+        if (!komik) {
+            return res.status(404).send({ message: 'Komik not found' });
+        }
+        await komik.update(data);
+        res.send ({ message: 'komik berhasil diupdate', komik });
+    } catch (error) {
+        res.status(500).send(error);
+    }
+});
+
+app.delete('/komik/:id', async (req, res) => {
+    const id = req.params.id;
+    try {
+        const komik = await db.Komik.findByPk(id);
+        if (!komik) {
+            return res.status(404).send({ message: 'Komik not found' });
+        }
+        await komik.destroy();
+        res.send({ message: 'komik berhasil dihapus', komik });
+    } catch (error) {
+        res.status(500).send(error);
+    }
+});
